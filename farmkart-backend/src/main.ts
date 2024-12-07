ad28fc7663629b77569387ef2116a820142e87c0
DevSuthar <123891197+Dev-Suthar-2002@users.noreply.github.com>
2024-12-07 14:08:03 -0500

    added Ionic server port

--- farmkart-backend/src/main.ts
+++ farmkart-backend/src/main.ts
@@ -7,7 +7,7 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Adjust this to your Next.js frontend URL
+    origin: ['http://localhost:3000','http://localhost:5173'], // Adjust this to your Next.js frontend URL
     credentials: true,
   });
 
